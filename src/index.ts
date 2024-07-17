a7e758c71243287f4b7535a48c44d881f28e8db4
Mauricio Ross <fxmross@gmail.com>
2024-07-17 17:54:26 -0400

    fix: add buys, translate to english and depracted attrs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 
-// export {Restaurant, TopSupplier, ChartData} from './restaurants/restaurants';
-// export { CostsCategory , CostsCategoryChild, FluffyChild, PurpleChild} from './restaurants/CostsCategory';
-import { Restaurant as Restaurant } from "./restaurants/restaurants"; 
-import { ChartData as ChartData, TopSupplier as TopSupplier, PriceList } from "./restaurants/restaurants";
-import { CostsCategory as CostsCategory } from "./restaurants/CostsCategory";
-export { Restaurant, CostsCategory, ChartData, TopSupplier, PriceList }
+
+export * from './buys';
+export * from './restaurants';
