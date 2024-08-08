128716e856e2a8850d577855f11ecd2145e1d2a2
Mauricio Ross <fxmross@gmail.com>
2024-08-08 00:23:42 -0400

    fix: text comnfig

--- src/buys/index.ts
+++ src/buys/index.ts
@@ -1,5 +1,5 @@
 /**
  * @module buys
- * @description This module contains all the types related to the buys.
+ * @description This module contains all the types related to the buys x.
  */
 export { Buy, Data, Receipt, Item, Category, CdgItem, Homologate, Image } from './buys';
