2df2bae61efac241250009338ce1fe1cc0f447d4
Mauricio Ross <fxmross@gmail.com>
2024-08-08 00:25:06 -0400

    fix: test husky config

--- src/buys/index.ts
+++ src/buys/index.ts
@@ -1,5 +1,5 @@
 /**
  * @module buys
- * @description This module contains all the types related to the buys x.
+ * @description This module contains all the types related to the buys.
  */
 export { Buy, Data, Receipt, Item, Category, CdgItem, Homologate, Image } from './buys';
