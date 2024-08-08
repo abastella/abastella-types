8ab6ca3334e0c507a4e27083217fc5dd3caeeab7
Mauricio Ross <fxmross@gmail.com>
2024-08-08 00:15:49 -0400

    fix: add description to module buys

--- src/buys/index.ts
+++ src/buys/index.ts
@@ -1,4 +1,5 @@
 /**
  * @module buys
+ * @description This module contains all the types related to the buys.
  */
 export { Buy, Data, Receipt, Item, Category, CdgItem, Homologate, Image } from './buys';
