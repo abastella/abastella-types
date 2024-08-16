b11ff369f7c300aea63ca51e089ae078e5402949
Mauricio Ross <fxmross@gmail.com>
2024-08-16 14:20:12 -0400

    fix: try fix error to export orm

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import * as admin from "firebase-admin";
 
 import * as fireorm from "fireorm";
 
-import { HomologationModel } from "./orm/restaurants/homologations";
-
 admin.initializeApp();
 const firestore = admin.firestore();
 
